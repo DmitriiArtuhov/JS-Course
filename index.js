7775939f532377b5c7f3ebf7a3f7a915233c287d
Artuhov Dmitrii <artuhovdmitrii@gmail.com>
2019-06-28 12:19:39 +0300

    Добавлено выполненное усложненное задание

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+/*--- Основное задание --------------------------------------------------------*/
+
 // #1
 
-let money = 1000000000000000000,  // доход за месяц
+let money = 1000,  // доход за месяц
 income = 'Freelance - 200000',
 addExpenses = 'Food, Apartment, Laptop',
 deposit = false,
-mission = 100000000000000000000000000000000000,
+mission = 100000000,
 period = 12;
 
 // #2
@@ -18,3 +20,22 @@ console.log(addExpenses.toLowerCase().split(', '));
 let budgetDay = money / 30;
 console.log(budgetDay);
 console.log(money % 30);
+
+/*--- Усложненное задание --------------------------------------------------------*/
+
+// #1
+
+let num = 266219;
+let mult = 1;
+
+while (num > 0) {
+	mult *= num % 10;  // Находит произведение цифр числа num
+	num = Math.floor(num / 10);  // Удаляет из числа num посчитанные цифры
+}
+
+console.log(mult);
+
+// #2
+
+mult **= 3; // возводит число в 3 степень
+console.log(mult % 100); // выводит первые 2 цифры полученного числа
