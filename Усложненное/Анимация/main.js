93dd5969d8241d30a1a8b9ffbc898c8755f0ff4e
Artuhov Dmitrii <artuhovdmitrii@gmail.com>
2019-07-16 22:29:04 +0300

    Выполнил основное и дополнительное домашнее задание

---
+++ Усложненное/Анимация/main.js
@@ -0,0 +1,50 @@
+let block = document.querySelector('#block'),
+		start = document.querySelector('#start'),
+		reset = document.querySelector('#reset'),
+		stop = document.querySelector('#stop');
+
+let id;
+
+const position = {
+	leftX: 0,
+	rightX: 600
+}
+
+function animate() {
+	id = requestAnimationFrame(animate);
+	if(position.leftX <= 600) {
+
+		position.leftX += 3;
+		block.style.left = position.leftX + 'px';
+
+	} else if(position.rightX >= 0) {
+
+		position.rightX -= 3;
+		block.style.left = position.rightX + 'px';
+
+	} else if(position.rightX < 0) {
+
+		position.leftX = 0;
+		position.rightX = 600;
+
+	}
+}
+
+start.addEventListener('click', function(e) {
+	e.preventDefault();
+
+	id = requestAnimationFrame(animate);
+});
+
+stop.addEventListener('click', function(e) {
+	e.preventDefault();
+
+	cancelAnimationFrame(id);
+});
+
+reset.addEventListener('click', function(e) {
+	e.preventDefault();
+
+	position.leftX = 0;
+	position.rightX = 0;
+});
